be5440d928d8aab74caf6c5aee6cdcb17ea92657
Devvara Rishivian <rishivian@gmail.com>
2017-12-17 17:20:10 +0700

    Templating 1

---
+++ application/controllers/Auth.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		//Do your magic here
+	}
+
+	public function index()
+	{
+		$this->load->view('view_home');
+	}
+
+}
+
+/* End of file controllername.php */
+/* Location: ./application/controllers/controllername.php */
