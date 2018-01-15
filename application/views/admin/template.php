76cc1997ed47bdba45ceae5cdcf457c5d1bd6243
Devvara Rishivian <rishivian@gmail.com>
2018-01-16 01:40:49 +0700

    fix bug petugas

--- application/views/admin/template.php
+++ application/views/admin/template.php
@@ -140,7 +140,7 @@
                   </a>
                   <ul class="dropdown-menu dropdown-usermenu pull-right">
                     <li><a href="javascript:;"> Profile</a></li>
-                    <li><a href="<?php echo base_url(); ?>index.php/admin/logout"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                    <li><a href="<?php echo base_url(); ?>index.php/petugas/logout"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
                   </ul>
                 </li>
               </ul>
